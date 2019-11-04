3d7c3a57fca96c375899fb529e015c5bdf5b9e4d
Zemlyanoy <tagambit@yandex.ru>
2019-11-04 17:48:08 +0000

    add preliminary constructions

--- src/chatDefaults.js
+++ src/chatDefaults.js
@@ -3,6 +3,7 @@ const chatDefaults = {
   authorName: 'localhost',
   messageInputText: 'Введите сообщение',
   sendMessageText: 'Send message',
+  chatName: 'Введите название'
 };
 
 export default chatDefaults;
---
+++ src/components/ChatCreationInput.js
@@ -0,0 +1,61 @@
+import React from 'react';
+
+/*const template = document.createElement('template');
+template.innerHTML = `
+    <style>
+        input {
+            border: 0;
+            border-radius: 20px;
+            outline: none;
+            width: calc(100% - 2px);
+            height: 100%;
+            background-color: #ffffcc;
+        }
+
+        :host {
+            display: inherit;
+            height: 100%;
+            border: 1px solid rgba(25, 25, 25, 0.32);
+            border-radius: 20px;
+            margin-bottom: 1px;
+        }
+    </style>
+    <input type="text">
+`;
+
+export default class ChatCreationInput extends HTMLElement {
+  constructor() {
+    super();
+    /* this.shadowRoot = /this.attachShadow({ mode: 'open' });
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+    this.$input = this.shadowRoot.querySelector('input');
+  }
+
+  static get observedAttributes() {
+    return ['name', 'value', 'placeholder', 'disabled'];
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    this.$input.setAttribute(name, newValue);
+  }
+
+  get value() {
+    return this.$input.value;
+  }
+
+  reset() {
+    this.$input.value = ''; // resets the form without reloading the page
+  }
+}
+
+customElements.define('chat-creation-input', ChatCreationInput);
+*/
+function ChatCreationInput(props) {
+    return (
+        <input className="chat-name-input" type="text" value={props.value}
+        onChange={props.onChange} placeholder={props.placeholder}
+        />
+    )
+}
+
+export default ChatCreationInput;
