998397e308159e3aac92c36bc4bc83416bf002f4
Zemlyanoy <tagambit@yandex.ru>
2019-11-16 12:38:29 +0000

    add simple scroll to the end

--- src/components/MessageHistory.js
+++ src/components/MessageHistory.js
@@ -1,26 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+// import { FlatList } from 'react-native';
 import PropTypes from 'prop-types';
 import MessageItem from './MessageItem';
 
 function MessageHistory({ messageArray }) {
-	return (
-		<div className="message-history-list">
-			{messageArray.map((item, index) => (
-				<li key={item.number}>
-					<MessageItem
-						author={item.author}
-						date={item.date}
-						number={item.number}
-						text={item.text}
-					/>
-				</li>
-			))}
-		</div>
-	);
+  // forced scroll to the end with every new message
+  useEffect(() => document.querySelector('#last').scrollIntoView(false), [
+    messageArray.length,
+  ]);
+  return (
+    <div className="message-history-list">
+      {messageArray.map((item, index) => (
+        <li key={item.number}>
+          <MessageItem
+            author={item.author}
+            date={item.date}
+            number={item.number}
+            text={item.text}
+          />
+        </li>
+      ))}
+      <div id="last" />
+    </div>
+  );
 }
 
 MessageHistory.propTypes = {
-	messageArray: PropTypes.arrayOf(PropTypes.object).isRequired,
+  messageArray: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 
 export default MessageHistory;
