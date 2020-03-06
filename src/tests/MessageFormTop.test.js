3aa9096c036a0bc292cb760483c3b7d7c9b64c66
Zemlyanoy <tagambit@yandex.ru>
2020-03-06 11:24:25 +0000

    remove extra imports from tests

--- src/tests/MessageFormTop.test.js
+++ src/tests/MessageFormTop.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Router, Route, Switch, Link } from 'react-router-dom';
+import { Router, Route, Switch } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
 import renderer from 'react-test-renderer';
 import MessageFormTop from '../components/MessageFormTop';
