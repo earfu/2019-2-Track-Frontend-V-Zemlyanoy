82382e4e89c9c5f037c62d865a2a7efd660d633b
Zemlyanoy <tagambit@yandex.ru>
2020-03-26 21:36:07 +0000

    add emoji translation to UTF symbols

--- src/emojiList.js
+++ src/emojiList.js
@@ -13,4 +13,19 @@ const emojiList = [
   'banknote-with-dollar-sign',
 ];
 
+export const emojiCodes = {
+  ':emoji-see-no-evil-monkey:': '0x1F648',
+  ':emoji-hear-no-evil-monkey:': '0x1F649',
+  ':emoji-speak-no-evil-monkey:': '0x1F64A',
+  ':emoji-ophiuchus:': '0x26CE',
+  ':emoji-volcano:': '0x1F30B',
+  ':emoji-fountain:': '0x26F2',
+  ':emoji-sleeping-symbol:': '0x1F4A4',
+  ':emoji-anger-symbol:': '0x1F4A2',
+  ':emoji-heavy-check-mark:': '0x2714',
+  ':emoji-cross-mark:': '0x274C',
+  ':emoji-exclamation-question-mark:': '0x2049',
+  ':emoji-banknote-with-dollar-sign:': '0x1F4B5',
+};
+
 export default emojiList;
