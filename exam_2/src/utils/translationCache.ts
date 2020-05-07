0c94ade08ab2aad6895ed4246821d441ba8bfdbd
Zemlyanoy <tagambit@yandex.ru>
2020-05-07 22:25:47 +0100

    try exam_2

---
+++ exam_2/src/utils/translationCache.ts
@@ -0,0 +1,19 @@
+import * as T from './types';
+
+// cache holds only while the session lasts
+let cache: T.Dictionary<T.cacheEntry> = {};
+
+export function checkCache(text: string): T.cacheEntry | undefined {
+  return cache[text];
+}
+
+export function setCache(
+  text: string,
+  lang: string,
+  response: T.APIJson,
+): void {
+  const entry: T.cacheEntry = [lang, response];
+  cache[text] = entry;
+  // console.log('Cached');
+  return;
+}
